507f1bccf3f63fc833c9897ec3792c519bda89d4
Martina <martinaa@medicarw.com.ar>
2022-08-18 09:21:20 -0300

    algoritmo 1

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,19 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  let numerosPrimos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
+  let numers = [1]
+  let i = 0;
 
+  while (num > 1) {
+    if (num % numerosPrimos[i] == 0) {
+      numers.push(numerosPrimos[i])
+      num = num / numerosPrimos[i]
+    } else {
+      i++;
+    }
+  }
+  return numers
 }
 
 function bubbleSort(array) {
@@ -14,7 +26,17 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let aux = [0];
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        aux = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = aux;
+      }
+    }
+  }
+  return array
 }
 
 
@@ -23,7 +45,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 1; i < array.length; i++) {
+    let j = i - 1;
+    let temp = array[i];
+    while (j >= 0 && array[j] > temp) {
+      array[j + 1] = array[j]
+      j--
+    }
+    array[j + 1] = temp
+  }
+  return array
 }
 
 
@@ -32,7 +63,20 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      let temp = array[i];
+      array[i] = array[min];
+      array[min] = temp;
+    }
+  }
+  return array
 }
 
 
