934a308ca1437e129ed4179f93c3917def956004
Martina <martinaa@medicarw.com.ar>
2022-08-18 11:52:58 -0300

    algoritmo 2

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,19 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) {
+    return array
+  }
+  let pivot = array.shift();
+  let left = [];
+  let right = [];
+  while (array.length) {
+    let item = array.shift()
+    item < pivot ? left.push(item) : right.push(item);
+  }
+  array = [...quickSort(left), pivot, ...quickSort(right)]
 
+  return array
 }
 
 function mergeSort(array) {
@@ -14,7 +26,23 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length < 2) return array
 
+  let mitad = Math.floor(array.length / 2)
+  let left = array.slice(0, mitad);
+  let right = array.slice(mitad, array.length)
+
+  array = []
+
+  left = mergeSort(left);
+  right = mergeSort(right);
+
+  while (left.length && right.length) {
+    left[0] < right[0] ? array.push(left.shift()) : array.push(right.shift());
+  }
+
+  array = array.concat(left, right)
+  return array
 }
 
 // No modificar nada debajo de esta línea
